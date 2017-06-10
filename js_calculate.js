47dee70b053a9b0e75d975f779941eee09ddae39
Ahmad-4A6C <505263@edu.rocmn.nl>
2017-06-10 15:40:38 +0200

    aan de style gewerkt, js klaar

---
+++ js_calculate.js
@@ -0,0 +1,35 @@
+document.getElementById("calculate").addEventListener("click", calculate);
+function calculate() {
+    str = document.getElementById('numbers').value;
+
+    charArray = str.split(",");
+
+    for (var i=0; i < charArray.length; i++) {
+        charArray[i] = parseFloat(charArray[i]);
+    }
+
+    oprationValue = document.getElementById('operation').value;
+
+    res = charArray[0];
+    for (var i=1; i < charArray.length; i++) {
+        res = calc(res, charArray[i], oprationValue);
+    }
+
+    // calculates result
+    function calc(firstNumber, secondNumber, operator) {
+        if (operator == '+') {
+            res = firstNumber + secondNumber;
+
+        } else if (operator == '-') {
+            res = firstNumber - secondNumber;
+
+        } else if (operator == '*') {
+            res = firstNumber * secondNumber;
+
+        } else if (operator == '/') {
+            res = firstNumber / secondNumber;
+        }
+        return res;
+    }
+    document.getElementById('result').innerHTML = res;
+}
